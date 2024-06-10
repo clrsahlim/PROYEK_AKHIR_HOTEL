0379db4d2720e7f149d826e2ece14a0729114804
clrsahlim <167694294+clrsahlim@users.noreply.github.com>
2024-06-10 23:34:29 +0700

    Update TugasAkhirLab_Hotel.cpp

--- TugasAkhirLab_Hotel.cpp
+++ TugasAkhirLab_Hotel.cpp
@@ -259,10 +259,7 @@ void CheckIn(char kodeKamar) {
             for (i = 0 ; i < data.jumlahKamar ; i++){
             nomorKamar [i] = 100 + room.availability;
             room.availability -= 1;
-<<<<<<< HEAD
-=======
         }
->>>>>>> e1c2c19c058a057543a717bc8ab183a98ced9f44
         }
         }
     }
@@ -305,12 +302,9 @@ void CheckIn(char kodeKamar) {
 
     ofstream file1(namaFile, ios::app);
     file1 << "Nama : " << data.nama << endl;
-<<<<<<< HEAD
     file1 << "\nWaktu Tanggal Check-in : " << ctime(&currentTime_t);
     //file1 << "Tanggal Check-In (dd/mm/yyyy) : " << data.tanggalCheckIn.tanggal << "/" << data.tanggalCheckIn.bulan << "/" << data.tanggalCheckIn.tahun << endl;
-=======
     file1 << "Tanggal Check-In (dd/mm/yyyy) : " << data.tanggalCheckIn.tanggal << "/" << data.tanggalCheckIn.bulan << "/" << data.tanggalCheckIn.tahun << endl;
->>>>>>> e1c2c19c058a057543a717bc8ab183a98ced9f44
     file1 << "NIK  : " << data.NIK << endl;
     file1 << "Harga Kamar : " << "Rp. " << harga * data.jumlahKamar * 3 / 2 + 150000 * data.bed << ",00" << endl;
     file1 << "Nomor Kamar : ";
@@ -399,24 +393,19 @@ int main() {
 
     else if (kegiatan == "quit") {
         cout << "Terima kasih, sampai jumpa!" << endl;
-<<<<<<< HEAD
         ulng = 'n';
-=======
         ulang = 0;
->>>>>>> e1c2c19c058a057543a717bc8ab183a98ced9f44
     } 
 
     else {
         cout << "Kegiatan Anda Tidak Terdaftar dalam Sistem. Mohon Input Ulang!\n";
         getchar();
-<<<<<<< HEAD
     }
     }
 
     return 0;
 }
 }
-=======
         ulang = 1;
     }
     }
@@ -425,4 +414,3 @@ int main() {
     return 0;
 }
 }
->>>>>>> e1c2c19c058a057543a717bc8ab183a98ced9f44
